aa1b1d3925b4cfac1f60f3b00fc9bbfc2e317bb3
SebasG03 <guamandavid11235@gmail.com>
2024-07-05 18:29:18 -0500

    Scripts added, TODO complete code

---
+++ Assets/Scripts/Game Management/CameraController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Reconoce si la camara se mueve o no.
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private float movementThreshold = 0.01f;
+    public Vector3 lastPosition;
+    public bool isMoving = false;
+
+    void Start()
+    {
+        lastPosition = Input.acceleration;
+    }
+
+    void Update()
+    {
+        Vector3 deltaPosition = Input.acceleration - lastPosition;
+        if (deltaPosition.sqrMagnitude > movementThreshold)
+        {
+            isMoving = true;
+        }
+        else
+        {
+            isMoving = false;
+        }
+        lastPosition = Input.acceleration;
+    }
+
+}
